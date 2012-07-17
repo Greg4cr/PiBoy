a2abe52abc8bc92f18efa2a3c7741ce544c1b04a
Gregory Gay <greg@greggay.com>
2012-07-17 16:19:38 -0500

    settings menu working

--- src/interface.py
+++ src/interface.py
@@ -1,3 +1,5 @@
+# Gregory Gay
+# PiBoy project
 # File: interface.py
 
 from Tkinter import *
@@ -39,8 +41,7 @@ class MainInterface (object):
 
 	self.settings = ImageTk.PhotoImage(file="../includes/settings.png")
 	self.settingsDispArea = canvas.create_image(((w/2.85)+112),((h/3)+100),image=self.settings,anchor=NW)
-	canvas.tag_bind(self.settingsDispArea,"<Button-1>",lambda x: self.launchGEdit())
-
+	canvas.tag_bind(self.settingsDispArea,"<Button-1>",lambda x: self.settingsMenu(canvas,w,h))
 
 	self.gb = ImageTk.PhotoImage(file="../includes/gb.png")
 	self.gbDispArea = canvas.create_image(w/2.85,((h/3)+200),image=self.gb,anchor=NW)
@@ -50,13 +51,47 @@ class MainInterface (object):
 	self.gbaDispArea = canvas.create_image(((w/2.85)+225),((h/3)+200),image=self.gba,anchor=NW)
 	canvas.tag_bind(self.gbaDispArea,"<Button-1>",lambda x: self.launchGEdit())
 
+	self.powerOff = ImageTk.PhotoImage(file="../includes/power.png")
+	self.powerDispArea = canvas.create_image(w-60,h-60,image=self.powerOff,anchor=NW)
+	canvas.tag_bind(self.powerDispArea,"<Button-1>",lambda x: self.shutdown())
+
     # Application launchers
     # Launches an application and closes the main GUI
     def launchGEdit(self):
 	system("gedit &")
 	exit()	
 
+    #Settings menu
+    def settingsMenu(self,canvas,w,h):
+	x0=w/3.5
+	x1=(w/3.5)+(w/2.5)
+	y0=h/3
+	y1=(h/3)+(h/2.5)
+
+	self.objects = []
+	#background
+	self.objects.append(canvas.create_rectangle(x0,y0,x1,y1,fill="white"))
+	
+	#back arrow
+	self.objects.append(ImageTk.PhotoImage(file="../includes/back.png"))
+	self.objects.append(canvas.create_image(x1-60,y1-60,image=self.objects[1],anchor=NW))
+	canvas.tag_bind(self.objects[2],"<Button-1>",lambda x: self.destroySettings(canvas,self.objects))
+
+	#Copyright text and header
+	self.objects.append(canvas.create_text(x0+5,y0+5,anchor=NW,font=("helvetica",16,"bold"),text="Settings"))
+	self.objects.append(canvas.create_text(x0+5,y1-10,anchor=SW,font=("helvetica",8),text="PiBoy Version 0.1 (Pong)\nDesigned by Gregory Gay (greg@greggay.com)\nPlease do not use this software to play games that you don't own!"))
+
+	#Options
+	self.objects.append(canvas.create_text(w/2.4,y0+50,anchor=NW,font=("helvetica",14,"bold"),text="Change Wallpaper",activefill="red"))
+		 
+    def destroySettings(self,canvas,objects):
+	for item in objects:
+	    canvas.delete(item)
+
+    #Shut system down
+    def shutdown(self):
+	exit()
+
 root = Tk()
 app = MainInterface(root)
-
 root.mainloop()
