cb0aeeebec3988787a798573d9b4319d6bc67e2e
Gregory Gay <greg@greggay.com>
2012-07-19 13:40:52 -0500

    can now change wallpaper

--- src/interface.py
+++ src/interface.py
@@ -8,6 +8,7 @@ import ImageTk
 import sys
 import getopt
 from os import system
+import subprocess as sub
 
 class MainInterface (object):
 
@@ -22,6 +23,10 @@ class MainInterface (object):
 	canvas = Canvas(master,width=w, height=h, bd=0, highlightthickness=0)
 	canvas.pack(expand='yes',fill='both')
 
+	self.createInterface(canvas,w,h)
+
+    #Draw interface
+    def createInterface(self,canvas,w,h):
 	#Set wallpaper
 	self.wall = ImageTk.PhotoImage(file="../includes/wallpaper/default.jpg")
 	self.background = canvas.create_image(0,0,image=self.wall,anchor=NW)
@@ -70,23 +75,141 @@ class MainInterface (object):
 
 	self.objects = []
 	#background
-	self.objects.append(canvas.create_rectangle(x0,y0,x1,y1,fill="white"))
-	
+	self.settingsBg = canvas.create_rectangle(x0,y0,x1,y1,fill="white")
+	self.objects.append(self.settingsBg)	
+
 	#back arrow
-	self.objects.append(ImageTk.PhotoImage(file="../includes/back.png"))
-	self.objects.append(canvas.create_image(x1-60,y1-60,image=self.objects[1],anchor=NW))
-	canvas.tag_bind(self.objects[2],"<Button-1>",lambda x: self.destroySettings(canvas,self.objects))
+	self.back = ImageTk.PhotoImage(file="../includes/back.png")
+	self.objects.append(self.back)	
+	self.backArrow = canvas.create_image(x1-60,y1-60,image=self.back,anchor=NW)
+	self.objects.append(self.backArrow)	
+
+	canvas.tag_bind(self.backArrow,"<Button-1>",lambda x: self.destroySettings(canvas,self.objects))
 
 	#Copyright text and header
 	self.objects.append(canvas.create_text(x0+5,y0+5,anchor=NW,font=("helvetica",16,"bold"),text="Settings"))
 	self.objects.append(canvas.create_text(x0+5,y1-10,anchor=SW,font=("helvetica",8),text="PiBoy Version 0.1 (Pong)\nDesigned by Gregory Gay (greg@greggay.com)\nPlease do not use this software to play games that you don't own!"))
 
 	#Options
-	self.objects.append(canvas.create_text(w/2.4,y0+50,anchor=NW,font=("helvetica",14,"bold"),text="Change Wallpaper",activefill="red"))
-		 
+	self.chooseBrightness = canvas.create_text(w/2.4,y0+50,anchor=NW,font=("helvetica",14,"bold"),text="Brightness",activefill="red")	
+	self.objects.append(self.chooseBrightness)	
+	canvas.tag_bind(self.chooseBrightness,"<Button-1>",lambda x: self.brightnessMenu(canvas,w,h,self.objects))
+	
+
+	self.chooseWallpaper = canvas.create_text(w/2.4,y0+75,anchor=NW,font=("helvetica",14,"bold"),text="Wallpaper",activefill="red")
+	self.objects.append(self.chooseWallpaper)	
+	canvas.tag_bind(self.chooseWallpaper,"<Button-1>",lambda x: self.wallpaperMenu(canvas,w,h,self.objects))
+
     def destroySettings(self,canvas,objects):
 	for item in objects:
 	    canvas.delete(item)
+
+    def destroyAndReturn(self,canvas,objects,w,h):
+	for item in objects:
+	    canvas.delete(item)
+
+	self.settingsMenu(canvas,w,h)
+
+    def brightnessMenu(self,canvas,w,h,oldObjects):
+	#erase settings menu
+	self.destroySettings(canvas,oldObjects)
+	
+	x0=w/3
+	x1=(w/3)+(w/3.4)
+	y0=h/2.5
+	y1=(h/2.5)+70
+
+	self.objects = []
+	#background
+	self.settingsBg = canvas.create_rectangle(x0,y0,x1,y1,fill="white")
+	self.objects.append(self.settingsBg)	
+
+	#back arrow
+	self.back = ImageTk.PhotoImage(file="../includes/back.png")
+	self.objects.append(self.back)	
+	self.backArrow = canvas.create_image(x1-60,y1-60,image=self.back,anchor=NW)
+	self.objects.append(self.backArrow)	
+
+	canvas.tag_bind(self.backArrow,"<Button-1>",lambda x: self.destroyAndReturn(canvas,self.objects,w,h))
+
+    def wallpaperMenu(self,canvas,w,h,oldObjects):
+	#erase settings menu
+	self.destroySettings(canvas,oldObjects)
+
+	#get number of images in the wallpaper directory
+	call = sub.Popen('ls ../includes/wallpaper/ | wc -w', stderr=sub.STDOUT, stdout=sub.PIPE, shell=True)
+	howManyWalls,error = call.communicate()
+	howManyWalls=int(howManyWalls)-1
+
+	x0=w/3.5
+	x1=(w/3.5)+(w/2.5)
+	y0=(h/2.5)-(100*int(howManyWalls/3))
+
+	if y0<0:
+		y0 = 0
+
+	y1=y0+275+(100*int(howManyWalls/3))
+
+	if y1 > h:
+		y1 = h
+
+	self.objects = []
+	#background
+	self.settingsBg = canvas.create_rectangle(x0,y0,x1,y1,fill="white")
+	self.objects.append(self.settingsBg)	
+
+	#back arrow
+	self.back = ImageTk.PhotoImage(file="../includes/back.png")
+	self.objects.append(self.back)	
+	self.backArrow = canvas.create_image(x1-60,y1-60,image=self.back,anchor=NW)
+	self.objects.append(self.backArrow)	
+
+	canvas.tag_bind(self.backArrow,"<Button-1>",lambda x: self.destroyAndReturn(canvas,self.objects,w,h))
+
+	#Current wallpaper
+	self.objects.append(canvas.create_text(x0+10,y0+60,anchor=NW,font=("helvetica",14,"bold"),text="Current:"))
+	self.currentWall= Image.open("../includes/wallpaper/default.jpg")
+	self.objects.append(self.currentWall)
+	self.currentWall.thumbnail((150,150),Image.ANTIALIAS)
+	self.currentThumb = ImageTk.PhotoImage(self.currentWall)
+	self.objects.append(self.currentThumb)
+	self.currentThumbDisp =canvas.create_image((w/2.4),y0+25,anchor=NW,image=self.currentThumb)
+	self.objects.append(self.currentThumbDisp)
+
+	self.objects.append(canvas.create_text(x0+10,y0+170,anchor=NW,font=("helvetica",14,"bold"),text="Choose Wallpaper:"))
+	
+	#Display wallpaper options
+	startX=x0+10
+	startY=y0+45
+	self.options=[]
+	self.optionThumbs=[]
+	self.optionThumbsDisp=[]
+
+	for i in range(0,(howManyWalls-1)):
+		#Update position
+		if i % 3 == 0:
+			startX=x0+10
+			startY=startY+160
+		else:
+			startX=startX+160
+
+		self.options.append(Image.open("../includes/wallpaper/wall"+str(i+1)+".jpg"))
+		self.objects.append(self.options[i])
+		self.options[i].thumbnail((150,150),Image.ANTIALIAS)
+		self.optionThumbs.append(ImageTk.PhotoImage(self.options[i]))
+		self.objects.append(self.optionThumbs[i])
+		self.optionThumbsDisp.append(canvas.create_image(startX,startY,anchor=NW,image=self.optionThumbs[i]))
+		self.objects.append(self.optionThumbsDisp[i])
+
+		canvas.tag_bind(self.optionThumbsDisp[i],"<Button-1>",lambda x,choice=i: self.changeWallpaper(canvas,w,h,choice))
+
+
+    def changeWallpaper(self,canvas,w,h,choice):
+	system("mv ../includes/wallpaper/default.jpg temp")
+	system("mv ../includes/wallpaper/wall"+str(choice+1)+".jpg ../includes/wallpaper/default.jpg")
+	system("mv temp ../includes/wallpaper/wall"+str(choice+1)+".jpg")
+	canvas.delete(ALL)
+	self.createInterface(canvas,w,h)
 
     #Shut system down
     def shutdown(self):
